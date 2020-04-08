c3c8b89e8ec0580d766ad9ef542416db7b878f0f
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-08 17:51:00 +0900

    Update Square.java

--- src/Square.java
+++ src/Square.java
@@ -7,6 +7,6 @@ public class Square extends Rectangle{
 	}
 
 	public void draw(int x,int y, int width) {
-		System.out.println("[正方形を描画] 点(" +x+ "," +y+ ")を基準として幅・高さ" +width+ "200の正方形");
+		System.out.println("[正方形を描画] 点(" +x+ "," +y+ ")を基準として幅・高さ" +width+ "の正方形");
 	}
 }
