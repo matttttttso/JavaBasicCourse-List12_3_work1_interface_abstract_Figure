ea6af84410cb23bee34d0325046d66a7cd6b2bb7
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-08 17:58:52 +0900

    Update Square.java

--- src/Square.java
+++ src/Square.java
@@ -2,10 +2,11 @@
 public class Square extends Rectangle{
 
 	//メソッドメンバ
-	public Square(int x,int y, int width) {
-
+	//スーパークラスRectangleのコンストラクタを明示的に呼び出す
+	public Square(int x,int y, int width, int height) {
+		super(x, y, width, height);
 	}
-
+	//出力
 	public void draw(int x,int y, int width) {
 		System.out.println("[正方形を描画] 点(" +x+ "," +y+ ")を基準として幅・高さ" +width+ "の正方形");
 	}
