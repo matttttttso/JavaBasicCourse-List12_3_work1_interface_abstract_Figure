c0fb10b4810db09735d881094dee88248d2eae87
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-08 17:55:02 +0900

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,21 @@
 
-public class Rectangle {
+public class Rectangle extends Polygon{
+	//フィールドメンバ
+	protected Point p;
+	protected int width;
+	protected int height;
 
+	//メソッドメンバ
+	//代入
+	public Rectangle(int x,int y, int width, int height) {
+
+	}
+	//出力
+	public void draw(int x,int y, int width, int height) {
+		System.out.println("[長方形を描画] 点(" +x+ "," +y+ ")を基準として幅" +width+ "、高さ" +height+ "の正方形");
+	}
+	//外周
+	public double perimeter() {
+		return ( width + height ) * 2;
+	}
 }
