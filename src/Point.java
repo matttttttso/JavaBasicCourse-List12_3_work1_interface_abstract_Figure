293e4f2b093c0a5fa93cd81380a5bd30810f7f0f
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-08 17:40:29 +0900

    Update Point.java

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,34 @@
-
 public class Point {
+	private int x;
+	private int y;
 
+	//xのゲッターセッター
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	//yのゲッターセッター
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	//引数なし
+	public Point() {
+		this.x = 0;
+		this.y = 0;
+	}
+
+	//引数２つ
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 }
