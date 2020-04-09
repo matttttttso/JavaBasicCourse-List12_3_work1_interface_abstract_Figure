aa269872c53582137c11332b2b29e6fb6f218856
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-04-09 11:33:44 +0900

    Autocorrect
    自動修正

--- src/Figure.java
+++ src/Figure.java
@@ -3,4 +3,6 @@ public interface Figure {
 	//メソッドメンバ
 	void draw();
 	double perimeter();
+	void draw(int x1, int y1, int x2, int y2);
+	double perimeter(int x1, int y1, int x2, int y2);
 }
--- src/Line.java
+++ src/Line.java
@@ -19,13 +19,27 @@ public class Line implements Figure {
 	}
 
 	//メソッドメンバ
+	@Override
 	public void draw(int x1, int y1, int x2, int y2) {
 		System.out.println("[線を描画] " +
 				"始点(" + x1 + "," + y1 + ")から" +
 				"終点(" + x2 + "," + y2 + ")まで");
 	}
-
+	@Override
 	public double perimeter(int x1, int y1, int x2, int y2) {
 		return Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
 	}
+
+	@Override
+	public void draw() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public double perimeter() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
 }
